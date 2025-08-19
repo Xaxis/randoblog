87c74724c61865b80d1e80a989f3cf61a30824c7
Xaxis <william.neeley@gmail.com>
2025-08-19 11:27:40 -0800

    New post

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,6 +2,7 @@ import { defineCollection, z } from 'astro:content';
 
 // GitHub repositories to fetch README content from
 const GITHUB_REPOS = [
+    'Xaxis/through-a-wormhole',
     'Xaxis/btc-ar-post',
     'Xaxis/detecting-the-graviton',
     'Xaxis/uapwatch-cubesat',
