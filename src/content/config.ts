3056d57b3f07c92655e54151fba09acebed70f47
Xaxis <william.neeley@gmail.com>
2025-09-01 10:43:40 -0800

    new article

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,6 +6,7 @@ const BLOG_TOPIC = 'randoblog';
 
 // Fallback list in case API fails (your current repos)
 const FALLBACK_REPOS = [
+  'Xaxis/fusion-in-2025',
   'Xaxis/an-open-console-that-could-actually-ship',
   'Xaxis/what-science-is-being-sat-on',
   'Xaxis/quantum-gravity-roadblocks-and-inroads',
